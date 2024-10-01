96c5b6f282a902496c12e57cf029566de54c7cd8
ImRaffy14 <raffysolis123@gmail.com>
2024-10-01 20:45:30 +0800

    added: changes on budget request controller

--- Controller/budgetRequestController.js
+++ Controller/budgetRequestController.js
@@ -6,9 +6,11 @@ const { encryptData } = require("../middleware/encryption")
 //GET ALL BUDGET REQUEST
 const getBudgetRequest = async (req, res) => {
     const requestData = await budgetRequestData.find({}).sort({ createdAt: -1})
-    const result = encryptData(requestData, process.env.ENCRYPT_DATA)
 
+    if(requestData){
+    const result = encryptData(requestData, process.env.ENCRYPT_DATA)
     res.status(200).json(result)
+    }
 }
 
 // POST BUDGET REQUEST FROM DIFFERENT SUBSYSTEM

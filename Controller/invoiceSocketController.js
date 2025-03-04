00c04d2a70247b83b19354bd320e653f92285851
ImRaffy14 <raffysolis123@gmail.com>
2025-03-05 01:41:22 +0800

    `Added functionality to send purchase order to Logistic 2 and updated error handling in invoiceSocketController.js`

--- Controller/invoiceSocketController.js
+++ Controller/invoiceSocketController.js
@@ -2,6 +2,10 @@ const invoiceRecords = require("../Model/invoiceRecordsModel")
 const { getPendingSalesData, getNonPendingRecords } = require("../Model/invoiceAggregation")
 const { totalLength, purchaseOrderDuplication } = require('../Controller/Anomaly-Detection/rule-based/detectDuplication')
 const orderInformationRecords = require('../Model/orderInformationModel')
+const { generateServiceToken } = require('../middleware/gatewayTokenGenerator')
+
+const axios = require('axios')
+
 
 module.exports = (socket, io) => {
 
@@ -40,13 +44,24 @@ module.exports = (socket, io) => {
             
             //RESPONSE INVOICE CREATED
             const response = await newInvoice.save()
-            if(response){
-                await orderInformationRecords.findOneAndDelete({ orderNumber: response.orderNumber})
-                const result = await orderInformationRecords.find({}).sort({ createdAt: -1})
-                const length = result.length
-                io.emit('receive_orders_length', length)
-                io.emit('receive_orders', result)
+            if(!response){
+                socket.emit("response_create_invoice", response)
             }
+
+            // SEND PURCHASE ORDER TO LOGISTIC 2
+            const token = generateServiceToken()
+            const sendPurchaseOrder = await axios.post('https://gateway.jjm-manufacturing.com/logistic2/purchase-order-update', response, {
+                headers: {Authorization: `Bearer ${token}`}
+            })
+            if(!sendPurchaseOrder){
+                return socket.emit('invoice_error', {message: 'Failed to send purchase order to Logistic 2'})
+            }
+
+            await orderInformationRecords.findOneAndDelete({ orderNumber: response.orderNumber})
+            const resultDelete = await orderInformationRecords.find({}).sort({ createdAt: -1})
+            const length = resultDelete.length
+            io.emit('receive_orders_length', length)
+            io.emit('receive_orders', resultDelete)
             socket.emit("response_create_invoice", response)
             
             //RESPONSE PENDING INVOICE
@@ -65,6 +80,7 @@ module.exports = (socket, io) => {
         }
         catch(err){
             console.log(err.message)
+            return socket.emit('invoice_error', {message: 'Server Error'})
         }
     }
 
